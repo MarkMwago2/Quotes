c484001ac999d43d33b3ead90ff12b5682a6766c
MarkMwago2 <markmwago@gmail.com>
2019-03-23 13:09:00 +0300

    generate component quotes-details

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    GoalDetailsComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule,
