bd60d4f4b49f1171ad58fac0be05eb1a4ea069b7
MarkMwago2 <markmwago@gmail.com>
2019-03-23 12:43:05 +0300

    create class component

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Quotes} from './quotes'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'Quotes';
-}
+    quotes = [
+      new Quotes(1,"Usiniambie, mimi Mtanzania bana."),
+      new Quotes(2,"Sit Down,Be humble"),
+      new Quotes(3,"Pambana na hali yako my friend"),
+    ]
+  }
