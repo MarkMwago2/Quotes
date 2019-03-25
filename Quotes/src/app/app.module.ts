25bcd576bc1cd873fe25718e3e94af2267ebbe05
MarkMwago2 <markmwago@gmail.com>
2019-03-25 11:25:31 +0300

    add form component

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component'
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuotesDetailsComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe,
+    QuoteComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
     
+
 
   ],
   providers: [],
